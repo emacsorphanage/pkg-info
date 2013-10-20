532495bbc5a85140b099f1de934f64e79e1b852e
Sebastian Wiesner <lunaryorn@gmail.com>
2013-10-20 17:46:49 +0200

    Bump master version

--- pkg-info.el
+++ pkg-info.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; URL: https://github.com/lunaryorn/pkg-info.el
 ;; Keywords: convenience
-;; Version: 0.3
+;; Version: 0.4-cvs
 ;; Package-Requires: ((dash "1.6.0") (epl "0.1"))
 
 ;; This file is not part of GNU Emacs.
